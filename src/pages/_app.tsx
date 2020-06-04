2057df1199b898f003af4c56b7c3390523cdf737
Huan Huang <wintersone@gmail.com>
2020-06-04 15:52:40 +0800

    modify author name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default ({ Component, pageProps }) => (
     <Component {...pageProps} />
     <footer>
       <abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.">CC BY-NC 4.0</abbr>
-      {` ${(new Date).getFullYear()} © Shu Ding.`}
+      {` ${(new Date).getFullYear()} © Huan Huang.`}
     </footer>
   </>
 )
