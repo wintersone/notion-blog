f8475b4d11b8b2ddaf902a42957b33c72cccd328
Huang Huan <wintersone@gmail.com>
2020-06-04 23:23:58 +0800

    modification

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+import Header from '../../components/header'
+import Content from '../../components/content'
+
+import { BLOG_INDEX_ID } from '../../lib/notion/server-constants'
+
+import getPageData from '../../lib/notion/getPageData'
+
+export async function unstable_getStaticProps() {
+  const postData = await getPageData(BLOG_INDEX_ID, 2)
+  const post = { content: postData.blocks }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 10,
+  }
+}
+
+export default ({ post }) => {
+  return (
+    <article>
+      <h1>Huan</h1>
+      <Header title="Huan" />
+      <Content blocks={post.content || []}/>
+    </article>
+  )
+}
